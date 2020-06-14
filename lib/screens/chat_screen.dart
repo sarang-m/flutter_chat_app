d8f961e0352fb307487f0afe2704e6f44ea76b43
sarang m <sarangsrg10@gmail.com>
2020-06-15 03:59:37 +0530

    chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _fireStore = Firestore.instance;
+FirebaseUser loggedInUser;
 
 class ChatScreen extends StatefulWidget {
 
@@ -17,7 +19,7 @@ class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
   String messageText;
-  FirebaseUser loggedInUser;
+
 
   @override
   void initState() {
@@ -120,16 +122,20 @@ class MessageStream extends StatelessWidget {
       builder: (context, snapshot){
         if (snapshot.hasData){
           List<MessageBubble>messageBubbles = [];
-          final messages = snapshot.data.documents;
+          final messages = snapshot.data.documents.reversed;
           for (var message in messages){
             final messageText = message.data['Text'];
             final messageSender = message.data['Sender'];
-
-            final messageBubble = MessageBubble(sender: messageSender,text: messageText,);
+            final currentSender = loggedInUser.email;
+            final messageBubble = MessageBubble(
+              sender: messageSender,
+              text: messageText,
+              isMe: currentSender==messageSender,);
             messageBubbles.add(messageBubble);
           }
           return Expanded(
             child: ListView(
+              reverse: true,
               padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 20),
               children: messageBubbles,
             ),
@@ -144,28 +150,33 @@ class MessageStream extends StatelessWidget {
 
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble({this.sender,this.text});
+  MessageBubble({this.sender,this.text,this.isMe});
   final String sender;
   final String text;
+  final bool isMe;
   @override
+
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: isMe ?CrossAxisAlignment.end:CrossAxisAlignment.start,
         children: <Widget>[
           Text(sender,
             style: TextStyle(fontSize: 12.0,color: Colors.black45),),
           SizedBox(height: 5.0,),
           Material(
             elevation: 5.0,
-            borderRadius: BorderRadius.circular(30.0),
-            color: Colors.lightBlueAccent,
+            borderRadius: isMe ? BorderRadius.only(
+                topLeft: Radius.circular(30),bottomLeft:Radius.circular(30),
+                bottomRight: Radius.circular(30.0)): BorderRadius.only(
+                bottomRight: Radius.circular(30.0),topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30)),
+            color: isMe ? Colors.lightBlueAccent : Colors.grey.shade200,
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 10.0),
-                child: Text('$text'),
-              ),
-            textStyle: TextStyle(fontSize: 15.0),),
+                child: Text('$text'),), textStyle: TextStyle(
+              color: isMe ? Colors.white:Colors.black54 ,fontSize: 15.0),),
         ],
       ),
     );
